ff8ad5b4c726be1d96a107e99cbec583de1da4ec
E100-cpu <126428428+E100-cpu@users.noreply.github.com>
2023-05-12 18:16:57 +1000

    Added files for back-up reasons
    Will probably be replaced with better versions.

---
+++ pietest/example.js
@@ -0,0 +1,45 @@
+function main() {
+	// d3 code goes here
+	var svg = d3.select('svg'),
+	width = svg.attr('width'),
+	height = svg.attr('height'),
+	radius = Math.min(width, height) / 2;
+	
+	var g = svg.append('g')
+			.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+	var color = d3.scaleOrdinal(['#C7CEEA','#B5EAD7','#FFDAC1', '#FF9AA2', '#42eff5', '#42f554', '#4254f5', '#f542e6' ]);
+	var pie = d3.pie().value(function(d){
+		return d.Net_overseas_migration_percent
+	})
+	var path = d3.arc()
+			.outerRadius(radius - 40)
+			.innerRadius(100);
+	var label = d3.arc()
+			.outerRadius(radius)
+			.innerRadius(radius - 150);
+
+	d3.csv('net_migration_capital_city_new_v2.csv').then(	
+		function(data){
+    console.log(data)
+		var arc = g.selectAll('.arc')
+		.data(pie(data))
+		.enter().append('g')
+		.attr('class', 'arc');
+        
+		arc.append('path')
+			.attr('d', path)
+			.attr('fill', function(d){return color(d.data.city)});
+
+		arc.append('text')
+			.attr('transform', function(d){return 'translate(' + label.centroid(d) + ')';})
+			.text(function(d){return d.data.Net_overseas_migration});
+            
+		svg.append('g')
+			.attr('transform', 'translate(' + (width / 2 - 120) + ',' + 20 + ')')
+			.append('text')
+            //text I think
+			.text('Net migrations from overseas')
+			.attr('class', 'title');
+		}
+	);
+}
